d2e0715804785e9259484294670fcc32e29d03b0
William <william@Williams-MacBook-Air.local>
2018-11-30 01:07:00 +0700

    Cleaning up

--- AES.py
+++ AES.py
@@ -23,6 +23,4 @@ sbox = [
 plaintext = "54776F204F6E65204E696E652054776F"
 key = "5468617473206D79204B756E67204675"
 
-rk = roundKey.generateRK(key)
-
-print(rk[10]) #round key selesai
+rk = roundKey.generateRK(key) #round key selesai
